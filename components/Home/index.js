7de2b2cd86540eaaee3840dd29cc9278696c1c6b
lnatu <dfvutien@gmail.com>
2021-08-13 21:05:10 +0700

    Add screen 5

--- components/Home/index.js
+++ components/Home/index.js
@@ -2,6 +2,7 @@ import Contact from 'components/Home/parts/Contact';
 import Screen1 from 'components/Home/parts/Screen1';
 import Screen3 from 'components/Home/parts/Screen3';
 import Screen4 from 'components/Home/parts/Screen4';
+import Screen5 from 'components/Home/parts/Screen5';
 import Screen8 from 'components/Home/parts/Screen8';
 
 const View = () => {
@@ -12,6 +13,8 @@ const View = () => {
       <Screen3 />
 
       <Screen4 />
+
+      {/* <Screen5 /> */}
 
       <Screen8 />
 
